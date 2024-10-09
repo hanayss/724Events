62105c434b106207f76037d5ecfddd7bbe2082f4
Hanayss <hanayssdev@gmail.com>
2024-10-09 14:39:04 +0200

    corrections formulaire

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -1,66 +1,77 @@
-import { useCallback, useState } from "react";
 import PropTypes from "prop-types";
+import { useCallback, useState } from "react";
+import Button, { BUTTON_TYPES } from "../../components/Button";
 import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
-import Button, { BUTTON_TYPES } from "../../components/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 500); })
+const mockContactApi = () =>
+    new Promise((resolve) => {
+        setTimeout(resolve, 500);
+    });
 
 const Form = ({ onSuccess, onError }) => {
-  const [sending, setSending] = useState(false);
-  const sendContact = useCallback(
-    async (evt) => {
-      evt.preventDefault();
-      setSending(true);
-      // We try to call mockContactApi
-      try {
-        await mockContactApi();
-        setSending(false);
-      } catch (err) {
-        setSending(false);
-        onError(err);
-      }
-    },
-    [onSuccess, onError]
-  );
-  return (
-    <form onSubmit={sendContact}>
-      <div className="row">
-        <div className="col">
-          <Field placeholder="" label="Nom" />
-          <Field placeholder="" label="Prénom" />
-          <Select
-            selection={["Personel", "Entreprise"]}
-            onChange={() => null}
-            label="Personel / Entreprise"
-            type="large"
-            titleEmpty
-          />
-          <Field placeholder="" label="Email" />
-          <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
-            {sending ? "En cours" : "Envoyer"}
-          </Button>
-        </div>
-        <div className="col">
-          <Field
-            placeholder="message"
-            label="Message"
-            type={FIELD_TYPES.TEXTAREA}
-          />
-        </div>
-      </div>
-    </form>
-  );
+    const [sending, setSending] = useState(false);
+    const [key, setKey] = useState(0);
+
+    const sendContact = useCallback(
+        async (evt) => {
+            evt.preventDefault();
+
+            setSending(true);
+            // We try to call mockContactApi
+            try {
+                await mockContactApi();
+                setSending(false);
+                onSuccess();
+
+                setKey((prevKey) => prevKey + 1);
+            } catch (err) {
+                setSending(false);
+                onError(err);
+            }
+        },
+        [onSuccess, onError]
+    );
+
+    return (
+        <form key={key} onSubmit={sendContact}>
+            <div className="row">
+                <div className="col">
+                    <Field placeholder="" label="Nom" />
+                    <Field placeholder="" label="Prénom" />
+                    <Select
+                        selection={["Personel", "Entreprise"]}
+                        onChange={() => null}
+                        label="Personel / Entreprise"
+                        type="large"
+                        titleEmpty
+                    />
+                    <Field placeholder="" label="Email" />
+                    <Button type={BUTTON_TYPES.SUBMIT} disabled={sending}>
+                        {sending ? "En cours" : "Envoyer"}
+                    </Button>
+                </div>
+                <div className="col">
+                    <Field
+                        placeholder="message"
+                        label="Message"
+                        type={FIELD_TYPES.TEXTAREA}
+                        name="message"
+                    />
+                </div>
+            </div>
+        </form>
+    );
 };
 
 Form.propTypes = {
-  onError: PropTypes.func,
-  onSuccess: PropTypes.func,
-}
+    onError: PropTypes.func,
+    onSuccess: PropTypes.func,
+};
 
 Form.defaultProps = {
-  onError: () => null,
-  onSuccess: () => null,
-}
+    onError: () => null,
+    onSuccess: () => null,
+};
 
 export default Form;
